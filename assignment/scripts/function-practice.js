7d81f2bc438822079f94b0a63f518f10567ca864
Chris Cantoni <chris.cantoni@gmail.com>
2023-08-29 16:42:38 -0500

    Function first draft solutions, DOM not yet implemented.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -10,52 +10,69 @@ function hello() {
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
-
+hello();
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return (`Hello, ${name}!`);
 }
 // Remember to call the function to test
-
+console.log(helloName('Chris'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber
 }
+console.log(addNumbers(7, 3));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(firstNum, secondNum, thirdNum) {
+  return firstNum * secondNum * thirdNum;
 }
 
+console.log(multiplyThree(4, 2, 3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-
+console.log(isPositive(-4));
+console.log(isPositive(18));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  return array[array.length - 1];
 }
+testArray = [2, 3, 10, 14, 'this is the last item in the array'];
+console.log(getLast(testArray));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+  let containsValue = false;
+  for (let item of array) {
+    if (item == value) {
+      containsValue = true;
+      return containsValue;
+    } else {
+      containsValue = false;
+    }
+  } return containsValue;
 }
+console.log(find(2, testArray)); // Should be true
+console.log(find(47, testArray)); // Should be false
+console.log(find(10, testArray)); // Should be true
 
 // ----------------------
 // Stretch Goals
@@ -63,25 +80,48 @@ function find(value, array) {
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+    if (string.indexOf(letter) == 0) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
+console.log('Does the letter start the string:', isFirstLetter('t', 'there'));
+console.log('Does the letter start the string:', isFirstLetter('w', 'happy'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll() {
+function sumAll(numbers) {
   let sum = 0;
   // TODO: loop to add items
-
+  for (let num of numbers) {
+    sum += num;
+  }
   // TODO: return the sum
+  return sum;
 }
+
+let sumArray = [4, 7, 20, 13, 15]; // total is 59
+let smallArray = [1, 2, 3]; // total is 6
+
+console.log(sumAll(sumArray));
+console.log(sumAll(smallArray));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {
-
+function allPositive(numArray) {
+  let positiveArray = [];
+  for (let num of numArray) {
+    if (num > 0) {
+      positiveArray.push(num);
+    } 
+  } return positiveArray;
 }
 
+let testNumArray = [6, 4, -3, 4, -1, 9];
+
+console.log(allPositive(testNumArray)); // output should be [6, 4, 4, 9]
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
